56346f3e870498ae27d31579495a1fd526de32f7
samratov1988 <samratov1988@gmail.com>
2021-03-27 12:37:48 +0600

    3.3. Разбор первой программы. [#505849]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Samat Samratov");
+        System.out.println("13.03.1988");
+    }
+}
