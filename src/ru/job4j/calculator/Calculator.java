dad3d05dd4920ef49f0c259c856b26dc13b80b82
samratov1988 <samratov1988@gmail.com>
2021-04-05 14:45:44 +0600

    0. Что такое тестирование [#505861]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -3,7 +3,7 @@ package ru.job4j.calculator;
 public class Calculator {
     public static void main(String[] args) {
         int one = 1;
-        int two =2;
+        int two = 2;
         int onePlusTwo = one + two;
         int six = 6;
         int four = 4;
