64827cf6eef67ce7ba36be358977ddc32024d571
samratov1988 <samratov1988@gmail.com>
2021-04-05 17:22:58 +0600

    4. Тесты для стороны прямоугольника. [#505865]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -8,7 +8,7 @@ public class SqArea {
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
+        double result1 = SqArea.square(7, 3);
         System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
     }
 }
