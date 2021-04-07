d3f4b0ee8a71517facc1e319d92e0adb5581667d
samratov1988 <samratov1988@gmail.com>
2021-04-07 21:13:54 +0600

    4.6.2. Switch - matcher [#505786]

--- test/ru/job4j/condition/MultipleSwitchWeekTest.java
+++ test/ru/job4j/condition/MultipleSwitchWeekTest.java
@@ -8,33 +8,33 @@ public class MultipleSwitchWeekTest {
     @Test
     public void whenMonThen1() {
         String day1 = "Monday";
+        String day2 = "Понедельник";
         int result1 = MultipleSwitchWeek.numberOfDay(day1);
+        int result2 = MultipleSwitchWeek.numberOfDay(day2);
         int expected = 1;
         Assert.assertEquals(expected, result1);
-        String day2 = "Понедельник";
-        int result2 = MultipleSwitchWeek.numberOfDay(day2);
         Assert.assertEquals(expected, result2);
     }
 
     @Test
     public void whenTuesThen2() {
         String day1 = "Tuesday";
+        String day2 = "Вторник";
         int result1 = MultipleSwitchWeek.numberOfDay(day1);
+        int result2 = MultipleSwitchWeek.numberOfDay(day2);
         int expected = 2;
         Assert.assertEquals(expected, result1);
-        String day2 = "Вторник";
-        int result2 = MultipleSwitchWeek.numberOfDay(day2);
         Assert.assertEquals(expected, result2);
     }
 
     @Test
     public void whenWedThen3() {
         String day1 = "Wednesday";
+        String day2 = "Среда";
         int result1 = MultipleSwitchWeek.numberOfDay(day1);
+        int result2 = MultipleSwitchWeek.numberOfDay(day2);
         int expected = 3;
         Assert.assertEquals(expected, result1);
-        String day2 = "Среда";
-        int result2 = MultipleSwitchWeek.numberOfDay(day2);
         Assert.assertEquals(expected, result2);
     }
 
