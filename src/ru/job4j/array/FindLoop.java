8e0aac73b6c8c119dfb321c958a2b12588263cbe
samratov1988 <samratov1988@gmail.com>
2021-04-12 22:32:15 +0600

    6.5. Поиск индекса в диапазоне. [#505803]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -17,6 +17,7 @@ public class FindLoop {
         for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 rst = i;
+                break;
             }
         }
         return rst;
